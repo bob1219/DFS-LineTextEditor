2806926ac1358bd01180abb27f62f47115d508ed
吉田大輝 <bob181219@yahoo.co.jp>
2018-02-25 17:29:22 +0900

    Describe dfs_lte::File::read function

---
+++ File.cpp
@@ -0,0 +1,23 @@
+// standard library
+#include <string>
+#include <iostream>
+#include <fstream>
+
+// header
+#include "class.h"
+
+// using
+using namespace std;
+
+void dfs_lte::File::read(const wstring& filename)
+{
+	wifstream file;
+	file.imbue(locale(""));
+	file.open(filename);
+	if(file.fail())
+		throw dfs_lte::exception(L"failed open file");
+
+	wstring line;
+	while(getline(file, line))
+		lines.push_back(line);
+}
