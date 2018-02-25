bdb2fb9c4d5e87c59bd63f5408700896f6cc420a
吉田大輝 <bob181219@yahoo.co.jp>
2018-02-25 16:54:07 +0900

    Describe a declaration of dfs_lte::File class

---
+++ class.h
@@ -0,0 +1,24 @@
+#ifndef CLASS_H
+#define CLASS_H
+
+// standard library
+#include <string>
+#include <list>
+
+namespace dfs_lte
+{
+	class File
+	{
+	public:
+		File();
+		File(const File&) = delete;
+		File(const std::wstring& filename);
+		void read(const std::wstring& filename);
+
+	private:
+		std::list<std::wstring> lines;
+		std::wstring filename;
+	};
+}
+
+#endif
