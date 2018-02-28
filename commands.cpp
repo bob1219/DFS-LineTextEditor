f3e46b4e2554f7f1eaf4aef5ecb3d809b0e52fe8
吉田大輝 <bob181219@yahoo.co.jp>
2018-02-28 21:15:12 +0900

    Describe dfs_lte::command::o function

---
+++ commands.cpp
@@ -0,0 +1,49 @@
+// standard library
+#include <string>
+
+// boost
+#include <boost/lexical_cast.hpp>
+
+// header
+#include "function.h"
+#include "class.h"
+
+// using
+using namespace dfs_lte;
+using namespace std;
+using namespace boost;
+
+void dfs_lte::command::o(Files& files, const wstring& fileno_s)
+{
+	try
+	{
+		int fileno = lexical_cast<int>(fileno_s);
+		File file = files.get(fileno);
+		file.open(file.getFilename());
+	}
+	catch(bad_lexical_cast& e)
+	{
+		throw dfs_lte::exception(L"invalid fileno");
+	}
+}
+
+void dfs_lte::command::o(Files& files, const wstring& fileno_s, const wstring& filename)
+{
+	if(fileno_s == L"-n")
+	{
+		File file = files.add();
+		file.open(filename);
+	}
+	else
+	{
+		try
+		{
+			int fileno = lexical_cast<int>(fileno_s);
+			File file = files.get(fileno);
+			file.open(filename);
+		}
+		catch(bad_lexical_cast& e)
+		{
+			throw dfs_lte::exception(L"invalid fileno");
+		}
+	}
