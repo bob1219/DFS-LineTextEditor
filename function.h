ffadabc1ad31c924a991d059011854cec1ede974
吉田大輝 <bob181219@yahoo.co.jp>
2018-02-28 21:07:58 +0900

    Modify declarations of command functions

--- function.h
+++ function.h
@@ -14,18 +14,18 @@ namespace dfs_lte
 
 	namespace command
 	{
-		void o(Files& files, const std::wstring& fileno);
-		void o(Files& files, const std::wstring& fileno, const std::wstring& filename);
-		void cl(Files& files, const std::wstring& fileno);
+		void o(Files& files, const std::wstring& fileno_s);
+		void o(Files& files, const std::wstring& fileno_s, const std::wstring& filename);
+		void cl(Files& files, const std::wstring& fileno_s);
 		void f(const Files& files);
-		void e(Files& files, const std::wstring& fileno, const std::wstring& lineno);
-		void a(Files& files, const std::wstring& fileno);
-		void i(Files& files, const std::wstring& fileno, const std::wstring& lineno);
-		void cp(Files& files, const std::wstring& fileno, const std::wstring& from_lineno, const std::wstring& to_lineno);
-		void w(const Files& files, const std::wstring& fileno);
-		void w(const Files& files, const std::wstring& fileno, const std::wstring& filename);
-		void as(Files& files, const std::wstring& fileno);
-		void r(Files& files, const std::wstring& fileno, const std::wstring& lineno);
+		void e(Files& files, const std::wstring& fileno_s, const std::wstring& lineno_s);
+		void a(Files& files, const std::wstring& fileno_s);
+		void i(Files& files, const std::wstring& fileno_s, const std::wstring& lineno_s);
+		void cp(Files& files, const std::wstring& fileno_s, const std::wstring& from_lineno_s, const std::wstring& to_lineno_s);
+		void w(const Files& files, const std::wstring& fileno_s);
+		void w(const Files& files, const std::wstring& fileno_s, const std::wstring& filename);
+		void as(Files& files, const std::wstring& fileno_s);
+		void r(Files& files, const std::wstring& fileno_s, const std::wstring& lineno_s);
 	}
 }
 
