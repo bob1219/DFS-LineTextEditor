054958d845ec6ed3e2733aab1ea6e93066473e34
吉田大輝 <bob181219@yahoo.co.jp>
2018-02-28 20:54:22 +0900

    Describe declarations of command functions

--- function.h
+++ function.h
@@ -11,6 +11,22 @@ namespace dfs_lte
 
 	[[noreturn]] void CommandLine(const Files& files);
 	void CommandProcess(const Files& files, const std::wstring& command);
+
+	namespace command
+	{
+		void o(const Files& files, const std::wstring& fileno);
+		void o(const Files& files, const std::wstring& fileno, const std::wstring& filename);
+		void cl(const Files& files, const std::wstring& fileno);
+		void f(const Files& files);
+		void e(const Files& files, const std::wstring& fileno, const std::wstring& lineno);
+		void a(const Files& files, const std::wstring& fileno);
+		void i(const Files& files, const std::wstring& fileno, const std::wstring& lineno);
+		void cp(const Files& files, const std::wstring& fileno, const std::wstring& from_lineno, const std::wstring& to_lineno);
+		void w(const Files& files, const std::wstring& fileno);
+		void w(const Files& files, const std::wstring& fileno, const std::wstring& filename);
+		void as(const Files& files, const std::wstring& fileno);
+		void r(const Files& files, const std::wstring& fileno, const std::wstring& lineno);
+	}
 }
 
 #endif
