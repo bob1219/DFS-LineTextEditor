7c2e474a367cde656dd00ce2db05523cd626582a
吉田大輝 <bob181219@yahoo.co.jp>
2018-03-05 20:59:30 +0900

    Describe dfs_lte::Files::list function

--- Files.cpp
+++ Files.cpp
@@ -1,9 +1,17 @@
+// standard library
+#include <iostream>
+
+// boost
+#include <boost/format.hpp>
+
 // header
 #include "Files.h"
 #include "File.h"
 
 // using
 using namespace dfs_lte;
+using namespace std;
+using namespace boost;
 
 File& dfs_lte::Files::get(unsigned int fileno) const
 {
@@ -26,4 +34,14 @@ void dfs_lte::Files::close(unsigned int fileno)
 		throw dfs_lte::exception(L"invalid fileno");
 
 	files.erase(files.begin() + --fileno);
+}
+
+void dfs_lte::Files::list() const
+{
+	int i = 1;
+	for(const File& file: files)
+	{
+		wcout << wformat(L"%1%:\t%2%") % i % file.getFilename() << endl;
+		++i;
+	}
 }
