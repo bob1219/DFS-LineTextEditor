accade6bfa58e2520e0ecde6b4878e70ede369fa
吉田大輝 <bob181219@yahoo.co.jp>
2018-03-10 11:06:45 +0900

    Describe wtokenizer.h

---
+++ wtokenizer.h
@@ -0,0 +1,16 @@
+#ifndef WTOKENIZER_H
+#define WTOKENIZER_H
+
+// standard library
+#include <string>
+
+// boost
+#include <boost/tokenizer.hpp>
+
+namespace dfs_lte
+{
+	using wseparator = boost::escaped_list_separator<wchar_t, std::char_traits<wchar_t>>;
+	using wtokenizer = boost::tokenizer<wseparator, std::wstring::const_iterator, std::wstring>;
+}
+
+#endif
