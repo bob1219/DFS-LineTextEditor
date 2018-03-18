c8e8eee1bc8f827f3c35e3c51613d5998622b160
吉田大輝 <bob181219@yahoo.co.jp>
2018-03-18 15:23:22 +0900

    Modify dfs_lte::CommandLine function

--- CommandLine.cpp
+++ CommandLine.cpp
@@ -33,7 +33,7 @@ using namespace boost;
 		}
 		catch(dfs_lte::exception& e)
 		{
-			wcerr << wformat(L"error: %1%.") % e.getMessage() << endl;
+			wcerr << wformat{L"error: %1%."} % e.getMessage() << endl;
 		}
 
 		wcout << endl;
